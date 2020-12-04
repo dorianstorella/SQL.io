fb74888b87da44a7e6829b814ec269c51354fe2a
Dorian Storella <dorianstorella@gmail.com>
2020-12-04 12:00:52 +0100

    point 3 fait reste 3 point

--- php-pdo/index.php
+++ php-pdo/index.php
@@ -1,7 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 
 <?php 
-
-
 try {
     $db = new PDO("mysql:host=localhost;dbname=weatherapp;charset=utf8",'root','');
     
@@ -13,9 +20,50 @@ $resultat = $db->query('SELECT * FROM météo');
 $donnees = $resultat->fetch();
 while ($donnees = $resultat->fetch())
 {
-    echo $donnees['ville'] ." ".$donnees["haut"]." ". $donnees["bas"] ."<br>";
+    echo $donnees['ville'] . " " . $donnees["haut"]." ". $donnees["bas"] ."<br>";
 }
 $resultat->closeCursor();
 ?>
 
-  
+<?php
+
+
+?>
+
+<table>
+    <tr>
+    <th>ville</th>
+    <th>haut</th>
+    <th>bas</th>
+    </tr>
+    <tr>
+    <!--   ici arrive sql -->   
+<?PHP
+    $resultat = $db->query('SELECT * FROM météo');
+$donnees = $resultat->fetch();
+while ($donnees = $resultat->fetch())
+{
+    echo "<tr>" . "<td>" . $donnees['ville'] . "</td>" . " " . "<td>" . $donnees["haut"] . "</td>" . " " . "<td> ". $donnees["bas"] ."</td>". "</tr>";
+}
+$resultat->closeCursor();
+?>    
+    <!--
+    <td>hey</td>
+    <td>hey</td>
+    <td>hey</td>
+    </tr>
+    <tr>
+    <td>hey</td>
+    <td>hey</td>
+    <td>hey</td>
+    </tr>
+    <tr>
+    <td>hey</td>
+    <td>hey</td>
+    <td>hey</td>
+    </tr>-->
+</table>
+
+
+</body>
+</html>  
